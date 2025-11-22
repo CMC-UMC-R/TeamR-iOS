53e46d3cb55f8c51cdb743838fe5ce44ffb50a1d
Lee Inho <inhoking01@gmail.com>
2025-11-22 16:06:50 +0900

    git init

---
+++ CMC-UMC-R/CMC_UMC_RApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMC_UMC_RApp.swift
+//  CMC-UMC-R
+//
+//  Created by 이인호 on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CMC_UMC_RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
