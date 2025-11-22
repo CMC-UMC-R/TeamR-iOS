58cb23ff457c590634410e64b540ba644a5b56b0
realhsb <realhsb@gmail.com>
2025-11-23 06:58:49 +0900

    feat: Mission api 추가

--- CMC-UMC-R/Sources/Core/Network/Target/MissionTarget.swift
+++ CMC-UMC-R/Sources/Core/Network/Target/MissionTarget.swift
@@ -10,8 +10,10 @@ import Moya
 import Alamofire
 
 enum MissionTarget {
-    case createMissions(CreateMissionRequest)                             /// 미션 생성
-    case getMissions(DayOfWeek)       /// 요일별 미션 조회
+    case createMissions(CreateMissionRequest)   /// 미션 생성
+    case getMissions(DayOfWeek)                 /// 요일별 미션 조회
+    case getDailyMission(DayOfWeek)             /// 요일별 미션 목록 조회 (3개 고정)
+    case getWeeklyStatus                        /// 이번 주 미션 달성 현황 조회
 }
 
 extension MissionTarget: BaseTargetType {
@@ -19,8 +21,12 @@ extension MissionTarget: BaseTargetType {
         switch self {
         case .createMissions:
             MissionAPI.createMissions.path
-        case .getMissions(let dayOfWeekType):
-            MissionAPI.getMissions(dayOfWeekType).path
+        case .getMissions(let dayOfWeek):
+            MissionAPI.getMissions(dayOfWeek).path
+        case .getDailyMission(let dayOfWeek):
+            MissionAPI.getDailyMission(dayOfWeek).path
+        case .getWeeklyStatus:
+            MissionAPI.getWeeklyStatus.path
         }
     }
     
@@ -30,6 +36,10 @@ extension MissionTarget: BaseTargetType {
                 .post
         case .getMissions:
                 .get
+        case .getDailyMission(_):
+                .get
+        case .getWeeklyStatus:
+                .get
         }
     }
     
@@ -37,11 +47,17 @@ extension MissionTarget: BaseTargetType {
         switch self {
         case .createMissions(let body):
             return .requestJSONEncodable(body)
-        case .getMissions:
-            return .requestPlain
             
-//            let parameters: [String: Any] = ["dayOfWeekType": dayOfWeekType.rawValue]
-//            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+        case .getMissions(let dayOfWeek):
+            let parameters: [String: Any] = ["dayOfWeek": dayOfWeek.rawValue]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+            
+        case .getDailyMission(let dayOfWeek):
+            let parameters: [String: Any] = ["dayOfWeek": dayOfWeek.rawValue]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+            
+        case .getWeeklyStatus:
+            return .requestPlain
         }
     }
 }
